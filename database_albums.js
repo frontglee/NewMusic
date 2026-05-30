9868d0bbeef0ac14ad7cf82be429e60c4778daa9
frontglee <ypelo400@gmail.com>
2026-05-30 20:42:11 +0200

    Initial release of NewMusic

---
+++ database_albums.js
@@ -0,0 +1,18 @@
+//albumType: "album","compilation","ep"
+const albumsDatabase = [
+{
+  albumID: "alb_tutorial",
+  title: "Tutorial",
+  artistID: "art_newmusic",
+  releaseDate: "2026",
+  firstlistenDate: "2026",
+  albumType: "album",
+
+  genre: [],
+  tags: [],
+
+  paths: {
+    cover: "./assets/newmusic/albums/tutorial.png"
+  }
+},
+];
